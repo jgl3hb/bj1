2abda02d4e39941acfca16cec2a4d20f6bac9ef6
jgl3hb <lewis.jeffreyg@gmail.com>
2022-09-03 14:09:12 -0400

    htmlcss

---
+++ app.js
@@ -0,0 +1,12 @@
+//Constants
+const bet1Btn = document.getElementById('bet-1')
+
+//Variables
+let deck = []
+let playerHand = []
+let dealerHand = []
+let burnpile = []
+let cardToRemove, cardPicked
+//Event Handlers
+
+//Function
